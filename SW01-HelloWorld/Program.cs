21a23c8908cbd59224c42544f6fd3ac4beda7950
Dominik Leimgruber <db.leimgruber@bluewin.ch>
2020-09-15 17:43:40 +0200

    init and hello world

---
+++ SW01-HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SW01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage("Hello World", true);
+        }
+
+        static void PrintMessage(String msg, bool toUpper)
+        {
+            if (toUpper)
+            {
+                msg = msg.ToUpper();
+            }
+            Console.WriteLine(msg);
+        }
+    }
+}
